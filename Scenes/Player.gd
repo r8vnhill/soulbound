ecb17cef92d224f4fedbd1dde7b16e1a356bbb38
R8V <islaterm@gmail.com>
2021-09-02 21:16:56 -0400

    feat(godot): Sprite movement | v0.5.1

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -1,28 +1,87 @@
 extends Area2D
 
-export var speed = 400
-var screen_size
+signal hit
 
+var velocity: Vector2
+var screen_size: Vector2
+export var speed := 400
+
+        
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	screen_size = get_viewport_rect().size
+    screen_size = get_viewport_rect().size
+    hide()
 
 
-func _process(delta):
-	var velocity = Vector2()
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite.play()
-	else:
-		$AnimatedSprite.stop()
-	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
+# Processes the input on every frame
+func _process(delta: float):
+    velocity = Vector2()
+    adjust_velocity()
+    move_player_sprite(delta)
+
+
+# region : PLAYER MOVEMENT
+
+# Adjusts the sprite velocity according to the user input
+func adjust_velocity():
+    velocity.x += (1 if Input.is_action_pressed(UserInput.RIGHT) else 
+                    -1 if Input.is_action_pressed(UserInput.LEFT) else 0)
+    velocity.y += (1 if Input.is_action_pressed(UserInput.DOWN) else 
+                    -1 if Input.is_action_pressed(UserInput.UP) else 0)
+
+
+# Moves the player according to its velocity
+func move_player_sprite(delta: float):
+    var sprite = $AnimatedSprite
+    if velocity.length() > 0:
+        velocity = velocity.normalized() * speed
+        sprite.play()
+    else:
+        sprite.stop()
+    sprite.position += velocity * delta
+    sprite.position.x = clamp(sprite.position.x, 0, screen_size.x)
+    sprite.position.y = clamp(sprite.position.y, 0, screen_size.y)
+
+    if velocity.x != 0:
+        sprite.animation = SpriteAnimation.WALK
+        sprite.flip_v = false
+        sprite.flip_h = velocity.x < 0
+    elif velocity.y != 0:
+        sprite.animation = SpriteAnimation.UP
+        sprite.flip_v = velocity.y > 0
+
+
+func start(pos):
+    position = pos
+    show()
+    $CollisionShape2D.disabled = false
+# endregion
+  
+      
+# region : MAPPINGS
+
+# Mapping of the input keys
+class UserInput:
+    const RIGHT = "ui_right"
+    const LEFT = "ui_left"
+    const DOWN = "ui_down"
+    const UP = "ui_up"
+
+
+class SpriteAnimation:
+    const WALK = "walk"
+    const UP = "up"
+    
+
+class Switch:
+    const ENABLED = "enabled"
+    const DISABLED = "disabled"
+# endregion
+
+
+# region : SIGNAL HANDLERS
+func _on_Player_body_entered(body: Node):
+    hide()  # Player disappears after being hit.
+    emit_signal($AnimatedSprite.hit.name)
+    $CollisionShape2D.set_deferred(Switch.DISABLED, true)
+# endregion
