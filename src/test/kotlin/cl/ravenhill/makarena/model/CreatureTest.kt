741795b2e4a70df32614bc5c6724c01749df34c1
R8V <islaterm@gmail.com>
2021-09-07 10:41:12 -0300

    test(creatures): Inequality test | v0.6.0.2

--- src/test/kotlin/cl/ravenhill/makarena/model/CreatureTest.kt
+++ src/test/kotlin/cl/ravenhill/makarena/model/CreatureTest.kt
@@ -1,9 +1,9 @@
-@file:Suppress("UnusedEquals")
-
 package cl.ravenhill.makarena.model
 
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import io.kotest.property.Arb
 import io.kotest.property.arbitrary.int
 import io.kotest.property.checkAll
@@ -15,23 +15,15 @@ class CreatureTest : StringSpec({
       Creature(name, atk1) shouldBe Creature(name, atk2)
     }
   }
-//  "Cards with different parameters are not equal" {
-//    val names = Pair(nameGenerator(), nameGenerator())
-//    val attacks = Pair(attackGenerator(), attackGenerator())
-//    forAll(names.first, names.second) { a, b ->
-//      if (a != b) {
-//        Card(a) != Card(b)
-//      } else {
-//        true
-//      }
-//    }
-//    forAll(attacks.first, attacks.second) { a, b ->
-//      if (a != b) {
-//        Card("name") != Card("name")
-//      } else {
-//        true
-//      }
-//    }
+  "Cards with different parameters are not equal" {
+    val names = Pair(nameGenerator(), nameGenerator())
+    val attacks = Pair(attackGenerator(), attackGenerator())
+    checkAll(names.first, names.second, attacks.first) { name1, name2, atk ->
+      if (name1 != name2) {
+        Creature(name1, atk) shouldNotBe Creature(name2, atk)
+      }
+    }
+  }
 })
 
 fun attackGenerator() = Arb.int(0, 10)
