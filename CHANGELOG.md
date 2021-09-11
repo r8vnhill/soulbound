fe67748fc47905b509dfb497a10a44c4674456ee
R8V <islaterm@gmail.com>
2021-09-11 12:55:59 -0300

    doc(summoning): Added missing test documentation | v0.6.1.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 # Changelog
 
-- snap(summoning): Test skeleton | ``v0.6.1.1``
+## Version 0.x
+
+### 0.6.x | Basic soulbounding
+- doc(soulbounding): Added missing test documentation | ``v0.6.1.4``
+- snap(soulbounding): Test skeleton | ``v0.6.1.1``
--- src/main/kotlin/cl/ravenhill/soulbound/cards/Creatures.kt
+++ src/main/kotlin/cl/ravenhill/soulbound/cards/Creatures.kt
@@ -1,5 +1,15 @@
 package cl.ravenhill.soulbound.cards
 
+/**
+ * Base interface for all Creature cards.
+ *
+ * @property isSummonable
+ *    flag to indicate if the Creature meets the soulbounding restrictions.
+ * @property level
+ *    the level of the card.
+ * @property name
+ *    the name of the card.
+ */
 sealed interface Creature {
   val isSummonable: Boolean
     get() = level.first == 1
@@ -7,6 +17,8 @@ sealed interface Creature {
   val name: String
 }
 
+/** A Spirit Creature.  */
 data class Spirit(override val name: String, override val level: Pair<Int, Int>) : Creature
 
+/** A Decay Creature. */
 data class Decay(override val name: String, override val level: Pair<Int, Int>) : Creature
